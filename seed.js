19c0b5af4a782d042c39dfc395d249a3e820d979
lgausman1 <l.gausman@comcast.net>
2015-05-28 18:08:32 -0700

    add seed

--- seed.js
+++ seed.js
@@ -9,7 +9,7 @@ db.Quiz.create({
   video: 'https://www.youtube.com/embed/CH1XGdu-hzQ?rel=0&amp;start=150&amp;end=182&amp;autoplay=1&amp;autohide=1&amp;showinfo=0',
   choices: 
    [ 'The name of the performer', 'A high-fashion shopping district in Seoul', 'A favorite South Korean food', 'None of the above' ]}
-{ question: 'Name that tune. Is it:',
+,{ question: 'Name that tune. Is it:',
   correctChoice: 1,
   video: 'http://www.youtube.com/embed/nrnZSLwfzVs?rel=0&amp;start=105&amp;end=141&amp;autoplay=1&amp;autohide=1&amp;showinfo=0',
   choices: 
@@ -18,7 +18,7 @@ db.Quiz.create({
      'No Woman, No Cry',
      'Moon River' ] }
 
-{ question: 'Name the performers. Is it:',
+,{ question: 'Name the performers. Is it:',
   correctChoice: 2,
   video: 'http://www.youtube.com/embed/zV2GA63HEGk?rel=0&amp;start=105&amp;end=146&amp;autoplay=1&amp;autohide=1&amp;showinfo=0',
   choices: 
@@ -27,17 +27,17 @@ db.Quiz.create({
      'Pharrell Williams and Daft Punk',
      'Fareed Zakaria and GPS' ] }
 
-{ question: 'Which state in the US was Madonna born in?',
+,{ question: 'Which state in the US was Madonna born in?',
   correctChoice: 3,
   video: 'https://www.youtube.com/embed/GuJQSAiODqI?rel=0&amp;start=99&amp;end=123&amp;autoplay=1&amp;autohide=1&amp;showinfo=0',
   choices: [ 'New York', 'Texas', 'Hawaii', 'Michigan' ] }
 
-{ question: 'Name this performer. Is it:',
+,{ question: 'Name this performer. Is it:',
   correctChoice: 1,
   video: 'https://www.youtube.com/embed/OPf0YbXqDm0?rel=0&amp;start=93&amp;end=141&amp;autoplay=1&amp;autohide=1&amp;showinfo=0',
   choices: [ 'James Brown, Jr.', 'Bruno Mars', 'Miguel', 'Wiz Khalifa' ] }
 
-{ question: 'Name the featured female vocalist. Is it:',
+,{ question: 'Name the featured female vocalist. Is it:',
   correctChoice: 3,
   video: 'https://www.youtube.com/embed/iEeD5G22Rqs?rel=0&amp;start=139&amp;end=184&amp;autoplay=1&amp;autohide=1&amp;showinfo=0',
   choices: 
